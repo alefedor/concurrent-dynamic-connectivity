dd0743058382fe74ae088fd3cccfa6b2ead210e4
Alexander Fedorov <fedorov2602@mail.ru>
2020-04-23 18:31:51 +0300

    Added incremental and decremental scenario generators

---
+++ src/main/kotlin/benchmarks/util/DecrementalScenarioGenerator.kt
@@ -0,0 +1,15 @@
+package benchmarks.util
+
+import kotlin.random.Random
+
+class DecrementalScenarioGenerator {
+    private val incrementalScenarioGenerator = IncrementalScenarioGenerator()
+
+    fun generate(graph: Graph, threads: Int): Scenario {
+        val queries = incrementalScenarioGenerator.generate(graph, threads).queries
+        for (threadQueries in queries)
+            for (i in threadQueries.indices)
+                threadQueries[i] = bidirectionalEdge(threadQueries[i].from(), threadQueries[i].to()).edgeToQuery(QueryType.REMOVE_EDGE)
+        return Scenario(graph.nodes, threads, graph.edges, queries)
+    }
+}
--- src/main/kotlin/benchmarks/util/IncrementalScenarioGenerator.kt
+++ src/main/kotlin/benchmarks/util/IncrementalScenarioGenerator.kt
@@ -4,7 +4,13 @@ import kotlin.random.Random
 
 class IncrementalScenarioGenerator {
     private val rnd = Random(647)
-    fun generate(graph: Graph, threads: Int) {
-
+    fun generate(graph: Graph, threads: Int): Scenario {
+        val edgesPerThread = graph.edges.size / threads
+        val queries = Array(threads) { thread ->
+            LongArray(edgesPerThread) {
+                graph.edges[thread * edgesPerThread + it].edgeToQuery(QueryType.ADD_EDGE)
+            }
+        }
+        return Scenario(graph.nodes, threads, LongArray(0), queries)
     }
 }
